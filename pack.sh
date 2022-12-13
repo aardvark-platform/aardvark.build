56bd1d0eaa3fa3bec26abbcbfb038bc3db4fb9b1
Harald Steinlechner <haraldsteinlechner@gmail.com>
2022-12-13 16:50:36 +0100

    fixed pack.sh

--- pack.sh
+++ pack.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
-dotnet build Aardvark.Build/Aardvark.Build.fsproj -c Debug
-dotnet pack Aardvark.Build/Aardvark.Build.fsproj -c Release -o bin/pack
+dotnet tool restore
+dotnet paket restore
+dotnet build Aardvark.Build\Aardvark.Build.fsproj -c Release
+dotnet build Aardvark.Build\Aardvark.Build.fsproj -c Debug
 dotnet pack aardpack/aardpack.fsproj -c Release -o bin/pack
+dotnet run --project getversion/getversion.fsproj
+version=`cat .version`
+echo "VERSION: $version"
+dotnet paket pack bin/pack --version $version --exclude Hans
