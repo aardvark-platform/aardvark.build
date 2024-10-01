1fc44743e81021e371bc239d824008c99c24c8f9
Martin <mautner@vrvis.at>
2024-10-01 15:47:37 +0200

    Fix pack.sh

--- pack.sh
+++ pack.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-source ./build.sh
+sh ./build.sh
 dotnet pack ./src/aardpack/aardpack.fsproj -c Release -o bin/pack
 
 dotnet publish ./src/Aardvark.Build/Aardvark.Build.fsproj -c Release -o bin/publish/standalone-tool
 
-version=`./bin/Release/net8.0/aardpack.exe --parse-only`
+version=`./bin/Release/net8.0/aardpack --parse-only`
 dotnet paket pack --template ./src/Aardvark.Build/paket.template --version $version bin/pack
